b495804a27878d3ec519b3d4480f215e90cd54d1
麻辣秃头 <evangelina4@126.com>
2018-09-13 00:40:54 +0800

    抓取图片-archer
    通过python爬虫抓取某论坛内图片

---
+++ Archer抓图.py
@@ -0,0 +1,31 @@
+import urllib.request
+import re
+
+
+def open_url(url):
+    req=urllib.request.Request(url)
+    req.add_header('User-Agent','Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36')
+    page=urllib.request.urlopen(req)
+    html=page.read().decode('utf-8')
+
+    return html
+
+
+def get_img(html):
+    p=r'<img class="BDE_Image" src="([^"]+\.jpg)"'
+    imglist=re.findall(p,html)
+
+    '''
+    for each in imglist:
+        print(each)
+    '''
+    for each in imglist:
+        filename=each.split("/")[-1]
+        urllib.request.urlretrieve(each,filename,None)
+
+
+
+
+if __name__=='__main__':
+    url='https://tieba.baidu.com/p/4922639233'
+    get_img(open_url(url))
